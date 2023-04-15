0e1daf706d6015f6b54b4ab14a125ea8df839a2d
auddl <10horysheet10@gmail.com>
2023-04-16 06:01:56 +0700

    Populate ViewModel and Dialogs

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -4,14 +4,43 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 
 class GameViewModel: ViewModel() {
-    init {
-        Log.d("GameFragment", "GameViewModel created!")
-    }
-    private var score = 0
+    private var wordList:MutableList<String> = mutableListOf()
+    private lateinit var currentWord:String
+    private var _score = 0
     private var currentWordCount = 0
-    private var _currentScrambledWord = "test"
+    private lateinit var _currentScrambledWord:String
+
+    val score:Int
+        get() = _score
     val currentScrambledWord:String
         get() = _currentScrambledWord
+    fun getNextWord()
+    {
+        currentWord = allWordsList.random()
+        val tempWord = currentWord.toCharArray()
+        while (String(tempWord).equals(currentWord, false)) {
+            tempWord.shuffle()
+        }
+        if(wordList.contains(currentWord))
+        {
+            getNextWord()
+        } else {
+            _currentScrambledWord = String(tempWord)
+            ++currentWordCount
+            wordList!!.add(currentWord)
+        }
+    }
+
+    fun nextWord():Boolean
+    {
+        return if (currentWordCount < MAX_NO_OF_WORDS){
+            getNextWord()
+            true } else false
+    }
+    init {
+        Log.d("GameFragment", "GameViewModel created!")
+        getNextWord()
+    }
 
     override fun onCleared() {
         super.onCleared()
